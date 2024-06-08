03d3cb1171f3a2076cf87d5bca68174bbe30acf4
Danish <danishsidd524@gmail.com>
2024-06-08 11:40:26 +0500

    feat: get all users controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import { getPaginatedUsers } from "../utils/helpers.js";
 
 export const getCurrentUser = async (req, res, next) => {
   try {
@@ -26,6 +27,36 @@ export const getSingleUser = async (req, res, next) => {
       success: true,
       message: "User fetched",
       data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const page = +(req.query.page || 1);
+    const limit = +(req.query.limit || 10);
+    const search = req.query.search || "";
+    const filter = req.query.filter || "";
+    let sortDirection = 1;
+
+    if (filter.toLowerCase() === "ztoa") {
+      sortDirection = -1;
+    }
+
+    const users = await getPaginatedUsers({
+      query: { username: { $regex: `^${search}`, $options: "i" } },
+      page,
+      limit,
+      sort: { username: sortDirection },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "All users",
+      data: users,
     });
   } catch (error) {
     console.log(error);
