9cf606f554fd99c9ba70a740906eb19a5d01ad1a
Danish <danishsidd524@gmail.com>
2024-06-20 01:06:23 +0500

    add: get all product controllers

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -16,3 +16,33 @@ export const getSingleProduct = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAllProducts = async (req, res, next) => {
+  try {
+    const page = +(req.query.page || 1);
+    const limit = +(req.query.limit || 10);
+    const search = req.query.search || "";
+    const filter = req.query.filter || "";
+    let sortDirection = 1;
+
+    if (filter.toLowerCase() === "ztoa") {
+      sortDirection = -1;
+    }
+
+    const products = await getPaginatedProducts({
+      query: { title: { $regex: `^${search}`, $options: "i" } },
+      page,
+      limit,
+      sort: { title: sortDirection },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "All products",
+      data: products,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
