af6e78ed0386281ed03b8d1b88cc991a2453166c
Danish <danishsidd524@gmail.com>
2024-06-30 14:54:30 +0500

    add: add review controller

--- server/src/controllers/ratings.controller.js
+++ server/src/controllers/ratings.controller.js
@@ -1,9 +1,43 @@
+import { Product } from "../models/product.model.js";
+import { User } from "../models/user.model.js";
+import { Review } from "../models/ratings.model.js";
+
+export const addReview = async (req, res, next) => {
+  try {
+    const { product, rating, review } = req.body;
+    if (!product) return next("Product is required");
+    if (!rating) return next("Rating is required");
+    if (req.user.role === "admin") return next("Admins cannot review products");
+
+    const existingReview = await Review.findOne({
+      product,
+      user: req.user._id,
+    });
+    if (existingReview) return next("You have already reviewed this product");
+
+    const newReview = await Review.create({
+      product,
+      rating,
+      review,
+      user: req.user._id,
+    });
+    if (!review) return next("Failed to add review");
+
+    return res.status(201).json({
+      success: true,
+      message: "Review added",
+      data: newReview,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 export const deleteReview = async (req, res, next) => {
-    try {
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  };
-  
+  try {
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
